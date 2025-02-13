0b924c4462713490d10becc85b58cb07c6e27fe6
tiffany.lan <ccc98112@gmail.com>
2025-02-13 16:12:45 +0800

    feat: use gps bundle to send message and create handlers

---
+++ src/MessageHandler/SmsNotificationHandler.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\MessageHandler;
+
+use App\Message\SmsNotification;
+use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+
+#[AsMessageHandler]
+class SmsNotificationHandler
+{
+    public function __invoke(SmsNotification $message)
+    {
+        echo $message->getContent() . ' handled' . PHP_EOL;
+    }
+}
